b2743745b604deda3474233c3da042c9bfb5e7e6
AndrewStanton94 <up683693@myport.ac.uk>
2014-11-18 13:04:01 +0000

    ReadFile annontation

--- ReadFile.java
+++ ReadFile.java
@@ -1,9 +1,9 @@
 import java.io.* ;
 public class ReadFile {
 	public static void main(String [] args) throws Exception {
-		FileInputStream in = new FileInputStream("N:/cosine/week9/tweedle-dum.txt");
-		byte buffer [] = new byte [100] ;
+		FileInputStream in = new FileInputStream("N:/cosine/week9/tweedle-dum.txt");	// Object with access to file content
+		byte[] buffer = new byte [100] ;		// Byte Array
 		int numBytesRead = in.read(buffer) ;
-		System.out.println(new String(buffer)) ;
+		System.out.println(new String(buffer)) ;	// Cast and output buffer content
 	}
 }
