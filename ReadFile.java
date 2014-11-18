63268e79a884fa7325dd57ec017340ecf7b9c688
AndrewStanton94 <up683693@myport.ac.uk>
2014-11-18 13:23:25 +0000

    ReadFile v2 (full content

--- ReadFile.java
+++ ReadFile.java
@@ -4,6 +4,10 @@ public class ReadFile {
 		FileInputStream in = new FileInputStream("N:/cosine/week9/tweedle-dum.txt");	// Object with access to file content
 		byte[] buffer = new byte [100] ;		// Byte Array
 		int numBytesRead = in.read(buffer);		// Store upto 100 bytes from in in buffer. Returns |bytes written|
-		System.out.println(new String(buffer)) ;	// Cast and output buffer content
+		while(numBytesRead > 0){ // While text yielded from file
+			System.out.print(new String(buffer, 0, numBytesRead));
+										// Data from, start index, |chars|
+			numBytesRead = in.read(buffer);
+		}
 	}
 }
